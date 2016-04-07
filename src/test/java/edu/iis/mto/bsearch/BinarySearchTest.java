d429961b83c2cc725ecb23b5ba1a0abe6ee23df2
Kadłubek <179640@edu.p.lodz.pl>
2016-04-07 14:33:21 +0200

    changed names of test methods

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -7,42 +7,42 @@ import static org.junit.Assert.*;
 public class BinarySearchTest {
 
     @Test
-    public void testSearchElementIsInOneElementSequence() throws Exception {
+    public void search_ElementIsInOneElementSequence() throws Exception {
         SearchResult searchResult = BinarySearch.search(1,new int[]{1});
         assertTrue(searchResult.isFound());
         assertTrue(searchResult.getPosition() == 0);
     }
 
     @Test
-    public void testSearchElementIsNotInOneElementSequence() throws Exception {
+    public void search_ElementIsNotInOneElementSequence() throws Exception {
         SearchResult searchResult = BinarySearch.search(1,new int[]{0});
         assertFalse(searchResult.isFound());
         assertTrue(searchResult.getPosition() == -1);
     }
 
     @Test
-    public void testSearchElementIsFirstInMultiElementSequence() throws Exception {
+    public void search_SearchElementIsFirstInMultiElementSequence() throws Exception {
         SearchResult searchResult = BinarySearch.search(1,new int[]{1,2,3});
         assertTrue(searchResult.isFound());
         assertTrue(searchResult.getPosition() == 0);
     }
 
     @Test
-    public void testSearchElementIsLastInMultiElementSequence() throws Exception {
+    public void search_ElementIsLastInMultiElementSequence() throws Exception {
         SearchResult searchResult = BinarySearch.search(3,new int[]{1,2,3});
         assertTrue(searchResult.isFound());
         assertTrue(searchResult.getPosition() == 2);
     }
 
     @Test
-    public void testSearchElementIsMiddleInMultiElementSequence() throws Exception {
+    public void search_ElementIsMiddleInMultiElementSequence() throws Exception {
         SearchResult searchResult = BinarySearch.search(2,new int[]{1,2,3});
         assertTrue(searchResult.isFound());
         assertTrue(searchResult.getPosition() == 1);
     }
 
     @Test
-    public void testSearchElementIsNotInMultiElementSequence() throws Exception {
+    public void search_ElementIsNotInMultiElementSequence() throws Exception {
         SearchResult searchResult = BinarySearch.search(4,new int[]{1,2,3});
         assertFalse(searchResult.isFound());
         assertTrue(searchResult.getPosition() == -1);
